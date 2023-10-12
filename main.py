85d47af2fa8d065206d946f226b9da9dc37be16e
Hem1700 <hemparekh1596@gmail.com>
2023-10-11 21:44:49 -0400

    Using exiftool to extract metadata

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-## Hello world
+import subprocess
+import json
+
+def print_metadata(image_path):
+    try:
+        # Run ExifTool to get metadata as JSON
+        command = ['exiftool', '-j', image_path]
+        result = subprocess.run(command, capture_output=True, text=True, check=True)
+
+        # Parse the JSON output
+        metadata = json.loads(result.stdout)[0]
+
+        # Print metadata
+        for tag, value in metadata.items():
+            print(f'{tag}: {value}')
+    except subprocess.CalledProcessError as e:
+        print('Error:', e)
+
+# Specify the image file path for which you want to print metadata
+image_path = 'image_path'
+
+# Call the function to print metadata
+print_metadata(image_path)
