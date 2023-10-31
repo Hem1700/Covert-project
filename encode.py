d511f2f7519b55c8f9fb186cdbbc8aac0911e964
Hem Parekh <54116609+Hem1700@users.noreply.github.com>
2023-10-31 19:30:40 -0400

    storing images in a directory after encoding

--- encode.py
+++ encode.py
@@ -2,40 +2,50 @@ import os
 import subprocess
 import shutil
 
-def encode_message_in_image(image_file, user_input):
+def encode_message_in_images(base_image, message, output_directory):
     try:
-        if len(user_input) < 4:
-            raise ValueError("Please enter at least four characters for encoding.")
+        # Check if the message is not empty
+        if not message:
+            raise ValueError("Please enter a message for encoding.")
 
-        # Get the ASCII values of the first four characters
-        ascii_values = [ord(char) for char in user_input[:4]]
+        # Split the message into segments of 4 characters each
+        segments = [message[i:i+4] for i in range(0, len(message), 4)]
 
-        # Create a copy of the original image to work with
-        temp_image_file = 'temp_image.jpg'
-        shutil.copy(image_file, temp_image_file)
+        # Create the output directory if it doesn't exist
+        os.makedirs(output_directory, exist_ok=True)
 
-        # Use exiftool to set ASCII values into specific metadata fields
-        exiftool_commands = [
-            '-Rating={}'.format(ascii_values[1]),
-            '-YResolution={}'.format(ascii_values[0]),
-            '-XResolution={}'.format(ascii_values[2]),
-            '-ExifVersion={}'.format(ascii_values[3])
-        ]
+        for i, segment in enumerate(segments):
+            # Create a copy of the base image to work with
+            temp_image = os.path.join(output_directory, f'image_{i}.jpg')
+            shutil.copy(base_image, temp_image)
 
-        subprocess.run(['exiftool'] + exiftool_commands + [temp_image_file])
+            # Get the ASCII values of the characters in the segment
+            ascii_values = [ord(char) for char in segment]
 
-        # Rename the temp image to the original image file name
-        os.rename(temp_image_file, image_file)
+            # Use exiftool to set ASCII values into specific metadata fields
+            exiftool_commands = [
+                '-Rating={}'.format(ascii_values[1]),
+                '-YResolution={}'.format(ascii_values[0]),
+                '-XResolution={}'.format(ascii_values[2]),
+                '-ExifVersion={}'.format(ascii_values[3])
+            ]
 
-        print("Metadata updated with ASCII values.")
+            subprocess.run(['exiftool'] + exiftool_commands + [temp_image])
+
+            print("Metadata updated with ASCII values for segment {}: {}".format(i, segment))
+
+        print("Message encoded successfully. Images stored in directory:", output_directory)
     except Exception as e:
         print("An error occurred:", e)
 
 # Get user input message
 user_input = input("Enter a message to encode: ")
 
-# Specify the image file
-image_file = 'img.jpg'  # Replace with your image file name
+# Specify the base image file
+base_image_file = 'img.jpg'  # Replace with your base image file name
+
+# Specify the output directory for storing encoded images
+output_directory = 'encoded_images'  # You can change this to your preferred directory name
 
 # Call the encoding function
-encode_message_in_image(image_file, user_input)
+encode_message_in_images(base_image_file, user_input, output_directory)
